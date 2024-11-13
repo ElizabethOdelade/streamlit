a68df59d0e00f93130427b856f5850774e674002
Olajide Geomeife <geomeifeodelade@gmail.com>
2024-11-13 09:00:15 +0100

    changes

--- dashboard.py
+++ dashboard.py
@@ -8,13 +8,7 @@ full_data = pd.read_csv(r"C:\Users\HP\Desktop\streamlit\data\full_data.csv")
 
 # Function to create the 'Churn by Gender' plot
 def dashboard_page(data):
-    # plt.figure(figsize=(10, 6))
-    # sns.countplot(x='gender', hue='Churn', data=data, palette='Dark2')
-    # plt.title('Churn by Gender')
-    # plt.xlabel('Gender')
-    # plt.ylabel('Count')
-    # st.pyplot(plt)  # Display the plot in Streamlit
-    # plt.clf()  # Clear the plot after displaying
+   
 
     # Total Customers vs. Churned Customers
     st.subheader("Total Customers vs. Churned Customers")
@@ -40,26 +34,7 @@ def dashboard_page(data):
 
     fig, ax = plt.subplots(2, 2, figsize=(15, 10))
 
-    # # Calculate mean charges grouped by churn status
-    # avg_charges = full_data.groupby("Churn")[["MonthlyCharges", "TotalCharges"]].mean().reset_index()
-
-    # # Plotting
-    # fig, ax = plt.subplots(1, 2, figsize=(15, 5))
-
-    # # Monthly Charges
-    # st.subheader("Monyhly")
-    # sns.barplot(x="Churn", y="MonthlyCharges", data=avg_charges, ax=ax[0], palette="muted")
-    # ax[0].set_title("Average Monthly Charges by Churn")
-    # ax[0].set_ylabel("Average Monthly Charges")
-
-    # # Total Charges
-    # sns.barplot(x="Churn", y="TotalCharges", data=avg_charges, ax=ax[1], palette="muted")
-    # ax[1].set_title("Average Total Charges by Churn")
-    # ax[1].set_ylabel("Average Total Charges")
-
-    # st.pyplot(fig)
-
-
+    
     # Monthly Charges Histogram for Churn = No
     st.subheader("Monthly and Total Charges")
     sns.histplot(full_data[full_data["Churn"] == "No"]["MonthlyCharges"], kde=True, ax=ax[0, 0], color="skyblue")
@@ -93,37 +68,4 @@ def dashboard_page(data):
     ax[1].set_title("Total Charges by Churn")
 
     st.pyplot(fig)
-    # # Billing Information: Monthly Charges and Total Charges
-    # st.header("Billing Information Analysis")
-    # fig, ax = plt.subplots(1, 2, figsize=(15, 5))
-
-    # # Monthly Charges
-    # sns.boxplot(x="Churn", y="MonthlyCharges", data=full_data, ax=ax[0])
-    # ax[0].set_title("Monthly Charges by Churn")
-
-    # # Total Charges
-    # sns.boxplot(x="Churn", y="TotalCharges", data=full_data, ax=ax[1])
-    # ax[1].set_title("Total Charges by Churn")
-
-    # st.pyplot(fig)
-
-# Function to create the 'Churn Rate for Senior Citizens vs. Non-Senior Citizens' plot
-# def plot_churn_by_senior_citizen(data):
-#     plt.figure(figsize=(10, 6))
-#     sns.countplot(x='SeniorCitizen', hue='Churn', data=data, palette='husl')
-#     plt.title('Churn Rate for Senior Citizens vs. Non-Senior Citizens')
-#     plt.xlabel('Citizenship Status')
-#     plt.ylabel('Count')
-#     st.pyplot(plt)  # Display the plot in Streamlit
-#     plt.clf()  # Clear the plot after displaying
-
-# # Streamlit app layout
-# st.title("Churn Analysis Dashboard")
-# st.subheader("Visualizations")
-
-# # Checkbox to display each plot
-# if st.checkbox("Show Churn by Gender"):
-#     dashboard_page(full_data)
-
-# if st.checkbox("Show Churn by Senior Citizenship"):
-#     plot_churn_by_senior_citizen(full_data)
+    
